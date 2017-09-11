fe7287876157e454e421a1a3d5aef9546e530a70
Greg Collins <gcollins@australianunity.com.au>
2017-09-11 22:36:40 +1000

    deployement working. fails to be called from postman

--- create-api.ps1
+++ create-api.ps1
@@ -1,4 +1,4 @@
-
-aws apigateway create-rest-api --name 'customer-retention-probability' --description 'Return the probability of a custer leaving the health fund' --profile personal
+aws apigateway delete-rest-api --rest-api-id 
+#aws apigateway create-rest-api --name 'customer-retention-probability' --description 'Return the probability of a custer leaving the health fund' --profile personal
 # Create Resource /customer-retention
-aws apigateway create-resource --name 'customer-retention'
+#aws apigateway create-resource --name 'customer-retention'
