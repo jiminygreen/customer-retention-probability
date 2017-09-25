b40d11117400aefedd02ac0c82a1bba2308e0485
jiminygreen <greg.collins@carsales.com.au>
2017-09-25 21:09:03 +1000

    Delete create-api.ps1

--- create-api.ps1
+++
@@ -1,4 +0,0 @@
-aws apigateway delete-rest-api --rest-api-id 
-#aws apigateway create-rest-api --name 'customer-retention-probability' --description 'Return the probability of a custer leaving the health fund' --profile personal
-# Create Resource /customer-retention
-#aws apigateway create-resource --name 'customer-retention'
